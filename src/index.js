4f4442155cb0a33a0854e6ab5734c813220ebf5c
srhodes <srhodes@DESKTOP-UV6CR3J.localdomain>
2023-05-15 10:10:19 -0700

    remove redux

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
+// import { Provider } from 'react-redux';
+// import { store } from './app/store';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
@@ -13,9 +13,11 @@ const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    {/* <Provider store={store}> */}
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    {/* </Provider> */}
   </React.StrictMode>
 );
 
